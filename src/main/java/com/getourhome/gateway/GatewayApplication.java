6bf7b102ea8a521a9cfc940edc5baaa21e142e3a
wonjun0120 <wonjundero@gmail.com>
2024-07-10 00:05:21 +0900

    feat: gateway server setting
    #2

--- src/main/java/com/getourhome/gateway/GatewayApplication.java
+++ src/main/java/com/getourhome/gateway/GatewayApplication.java
@@ -2,12 +2,24 @@ package com.getourhome.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @RestController
+    public static class HealthCheckController {
+        @GetMapping("/actuator/health")
+        public String health() {
+            return "OK";
+        }
     }
 
 }
